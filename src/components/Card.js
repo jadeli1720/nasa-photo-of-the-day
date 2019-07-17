484ed46911eab3d9e7569c400ba4be9c1bb8b35b
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-07-17 14:07:43 -0600

    Added a components folder with two component files: Cards, and Images

---
+++ src/components/Card.js
@@ -0,0 +1,8 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+
+// https://api.nasa.gov/api.html#apod
+export default function CardContent() {
+    const [image, set]
+}
