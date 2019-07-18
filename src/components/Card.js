4995b1bb6f34dd9ffce0a18943cb9f5d4e55386a
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-07-18 12:43:20 -0600

    Refactoring Card.js and data.js. Want to streamline componenets, props and useStates

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,8 +2,10 @@ import React from "react";
 
 
 export default function Card({info}) {
+    console.log(info)
     return (
         <div className="card">
+            <img src={info.url} alt={info.title}/>
             <h1>{info.title}</h1>
             <div>{info.explanation}</div>
             <p>{info.date}</p>
--- src/components/data.js
+++ src/components/data.js
@@ -2,20 +2,16 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Card from './Card';
 
-//
+//https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2015-03-17
 // https://api.nasa.gov/planetary/apod?api_key=cneyY3k8YLbbXoGH4H6xR1Nj410rdjBW96m4wSJy&date=2019-05-14
 export default function Data() {
-    const [data, setData] = useState([]);
-    // const [image, setImage] = useState('Image');
-    // const [title, setTitle] = useState('A title');
+    const [data, setData] = useState({});
 
     useEffect(() => {
-        axios.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2015-03-17`)
-            .then(response => {
-                setData(response.data);
-                // setTitle(response.data.title);
-                // setImage(response.data.url[0]);
-                console.log('response', response);
+        axios.get(`https://api.nasa.gov/planetary/apod?api_key=cneyY3k8YLbbXoGH4H6xR1Nj410rdjBW96m4wSJy&date=2019-05-14`)
+            .then(res => {
+                setData(res.data);
+                console.log('response', res);
             })
             .catch(error => {
                 console.log('error', error);
@@ -26,8 +22,6 @@ export default function Data() {
     // if (!data) return <h3>Loading...</h3>;
     return (
         <div className="cardBody">
-            {/* <Media info={data}/> */}
-            <img src={data.url}  alt={data.title}></img>
             <Card info={data}/>
         </div>
     )
